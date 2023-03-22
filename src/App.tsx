c9bca8d39e944a1b1475fbfc6b6326d6034f2771
Jesús Macías Félix <maciasfelixj@gmail.com>
2023-03-22 08:22:26 +0000

    chore - init and clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,15 @@
-import "./styles.css";
-import {
-  NativeBaseProvider,
-  Box,
-  Image,
-  Text,
-  Link,
-  HStack,
-  Heading,
-  Switch,
-  useColorMode,
-  VStack,
-  Code
-} from "native-base";
-import theme, { ITheme } from "./theme";
-
-// Making the custom theme typing available
-declare module "native-base" {
-  interface ICustomTheme extends ITheme {}
-}
-
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2}>
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light" ? true : false}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
-}
+import { NativeBaseProvider, Box, Heading, VStack } from "native-base";
 
 export default function App() {
+  const response = fetch("https://dummyjson.com/posts").then(
+    (data: unknown) => data
+  );
+
   return (
-    <NativeBaseProvider theme={theme}>
-      <Box
-        _light={{ bg: "coolGray.50" }}
-        _dark={{ bg: "coolGray.900" }}
-        minHeight="100vh"
-        justifyContent="center"
-        px={4}
-      >
-        <VStack space={5} alignItems="center">
-          <Image
-            source={{
-              uri: "https://docs.nativebase.io/img/nativebaselogo.svg"
-            }}
-            alt="NativeBaseLogo"
-            h="150"
-            w="132"
-          />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <Text>
-            Edit <Code>src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
+    <NativeBaseProvider>
+      <Box padding={4}>
+        <VStack alignItems="center">
+          <Heading size="lg">Postify</Heading>
         </VStack>
       </Box>
     </NativeBaseProvider>
